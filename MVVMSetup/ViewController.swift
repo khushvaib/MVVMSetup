a6c71f4da0366abf69384c77648b35842a18f99e
Vaibhav <vaibhavsharma.cs@gmail.com>
2021-05-16 16:06:23 +0530

    Initial Commit

---
+++ MVVMSetup/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMSetup
+//
+//  Created by Vaibhav Sharma on 16/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
